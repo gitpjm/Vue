71a14192fddb01708be1cc30963b37e6d432747b
pjm <1570025663@qq.com>
2018-01-23 08:24:47 +0800

    虚拟Dom

---
+++ 1/php/php.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+</head>
+<body>
+<?php
+$a = 'welcom';
+$b = '123';
+ echo '$a$b:' .$a.$b.
+?>
+</body>
+</html>
