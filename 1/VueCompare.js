6e8267eaa24664ffb466ac9dfa1cf2ee5740b964
pjm <1570025663@qq.com>
2018-01-25 16:23:33 +0800

    策略模式合并数据

--- 1/VueCompare.js
+++ 1/VueCompare.js
@@ -1,3 +1,6 @@
+/*
+https://github.com/gitpjm/Vue
+*/
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
